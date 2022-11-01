2d030aac91e28f6e244ecf335af840f94f453729
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-11-01 05:03:11 +0000

    footer section

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,11 @@
-import gsap from "gsap";
+import gsap, { random } from "gsap";
 
 export default function Animate() {
   const elements = document.querySelectorAll(".cursor-hover");
   elements.forEach((element) => {
 
     element.addEventListener("mouseenter", () => {
-      gsap.set(".cursor", { opacity: 1, delay:0.3 });
+      gsap.set(".cursor", { opacity: 1, delay: 0.3 });
       element.addEventListener("mousemove", (e) => {
         const mouse = { x: e.pageX, y: e.pageY };
         const pos = {
@@ -22,6 +22,14 @@ export default function Animate() {
 
   gsap.to(".bar", {
     height: "random(30, 100)",
+    duration: 0.8,
+    stagger: 0.2,
+    ease: "power2.easeIn",
+    repeat: Infinity,
+    yoyo: true,
+  });
+  gsap.to(".verticle", {
+    y: "random(50,100)" + "px",
     duration: 0.8,
     stagger: 0.2,
     ease: "power2.easeIn",
